2204e9b1bd771de25b6669c2a3df1a4b3b31a893
shoryabansalgithub <bansalshorya13@gmail.com>
2025-01-11 06:39:02 +0530

    add file

---
+++ 01basicreact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Chai from "../chai"
+
+function App() {
+  
+  return (
+    <>
+      <Chai/>
+    
+    </>
+  
+ 
+  )
+}
+
+export default App
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function App() {
+  let counter = 0; // Declare and initialize the counter variable
+
+const addValue = () => {
+    console.log("clicked"); // Log the message when the function is called
+    counter += 1; // Increment the counter by 1
+};
+
+
+
+  
+}
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>counter value :5</h2>
+      <button
+      onClick={addValue}>
+        
+        add value</button>
+      <br />
+      <button>remove</button>
+    </>
+  )
+
+
+export default App
